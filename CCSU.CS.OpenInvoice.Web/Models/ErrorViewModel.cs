ce45c2bf1f3e14d13ca9aa9e87ef6d534799379d
yassir-y <90559530+yassir-y@users.noreply.github.com>
2023-09-29 19:24:19 -0400

    Add project files.

---
+++ CCSU.CS.OpenInvoice.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CCSU.CS.OpenInvoice.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
