4cb0969d4c42e54ddb499013e9d79f23ca8f22af
Julianna Shevchenko <juliannashevchenko@Juliannas-MacBook-Air.local>
2023-10-01 21:41:03 -0400

    working customer data grid

---
+++ CCSU.CS.OpenInvoice.Web/Controllers/CustomerAPIController.cs
@@ -0,0 +1,21 @@
+﻿using CCSU.CS.OpenInvoice.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCSU.CS.OpenInvoice.Web.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CustomerAPIController : ControllerBase {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Customer customerInfo) {
+
+            //if(ModelState.IsValid) {
+            //    return View("SuccessValidation");
+            //}
+
+            return Ok(customerInfo);
+        }
+    }
+}
+
