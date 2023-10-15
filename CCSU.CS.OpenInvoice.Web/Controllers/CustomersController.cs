5a5dd1e5edbfa01b2f17df56f4032404d3410022
Jesse Burlison <JesseBurlison@gmail.com>
2023-10-15 15:41:38 -0400

    Added simple ajax call to json object for data table

---
+++ CCSU.CS.OpenInvoice.Web/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿//using CCSU.CS.OpenInvoice.Web.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace CCSU.CS.OpenInvoice.Web.Controllers
+//{
+//    [Route("api/[controller]")]
+//    public class CustomersController : ControllerBase
+//    {
+//        private readonly InvoicingContext _invoicingContext;
+//        public CustomersController(InvoicingContext invoicingContext)
+//        {
+//            _invoicingContext = invoicingContext;
+//        }
+//        [HttpGet]
+//        public async Task<ActionResult<Customer>> Get()
+//        {
+
+//        }
+//    }
+//}
--- CCSU.CS.OpenInvoice.Web/wwwroot/js/Customer.js
+++ CCSU.CS.OpenInvoice.Web/wwwroot/js/Customer.js
@@ -1,1 +1,16 @@
-const table = new DataTable('#example');
+$('#example').DataTable({
+    ajax: './js/SampleCustomers.txt',
+    columns: [
+        { data: 'CustomerId' },
+        { data: 'Name' },
+        { data: 'Contact'},
+        { data: 'Address' },
+        { data: 'Address2' },
+        { data: 'City' },
+        { data: 'State' },
+        { data: 'Zip' },
+        { data: 'Country' },
+        { data: 'Phone' },
+        { data: 'Email' }
+    ]
+});
