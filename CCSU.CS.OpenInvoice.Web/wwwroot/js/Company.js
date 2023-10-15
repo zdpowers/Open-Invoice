d8ea580ab05455608608d86012c0703a9bcc339e
yassir-y <90559530+yassir-y@users.noreply.github.com>
2023-10-15 07:32:51 -0400

    Removed DevExpress library Working on Company Screen using Bootstrap and Jquery

--- CCSU.CS.OpenInvoice.Web/wwwroot/js/Company.js
+++ CCSU.CS.OpenInvoice.Web/wwwroot/js/Company.js
@@ -1,5 +1,5 @@
 ﻿let companyFormInstance;
-let companyLogo; 
+let companyLogo;
 
 function companyForm_onInitialized(e) {
     companyFormInstance = e.component;
@@ -35,43 +35,118 @@ function fileUploader_onValueChanged(e) {
     }
 }
 
-function companyForm_onSubmit(e) {
+document.addEventListener("change", function (event) {
+    if (event.target.classList.contains("uploadProfileInput")) {
+        var triggerInput = event.target;
+        var currentImg = triggerInput.closest(".pic-holder").querySelector(".pic")
+            .src;
+        var holder = triggerInput.closest(".pic-holder");
+        var wrapper = triggerInput.closest(".profile-pic-wrapper");
 
-    e.event.preventDefault();
-
-    let data = companyFormInstance.option("formData");
-    let headers = {};
-    let antiForgeryToken = $("input[name=__RequestVerificationToken]").val();  
-    let validationResult = companyFormInstance.validate();
-    if (validationResult.isValid) {
-        headers['RequestVerificationToken'] = antiForgeryToken;
-        //data.Logo = companyLogo;
-        $.ajax({
-            url: "api/Companies/save",
-            headers: headers,
-            contentType: "application/json",
-            type: "POST",
-            dataType: "json",
-            data: JSON.stringify(data),
-            success: function (data) {
-                $("#company-toast").dxToast("instance").option({
-                    type: "success",
-                    message: "Company record has been updated successfully!",
-                    visible: true
-                });
-            },
-            error: function (xhr, textStatus, errorThrown) {
-                $("#company-toast").dxToast("instance").option({
-                    type: "error",
-                    message: errorThrown,
-                    visible: true
-                });
-                alert('Request Status: ' + xhr.status + '; Status Text: ' + textStatus + '; Error: ' + errorThrown);
-            }
+        var alerts = wrapper.querySelectorAll('[role="alert"]');
+        alerts.forEach(function (alert) {
+            alert.remove();
         });
 
+        triggerInput.blur();
+        var files = triggerInput.files || [];
+        if (!files.length || !window.FileReader) {
+            return;
+        }
+
+        if (/^image/.test(files[0].type)) {
+            var reader = new FileReader();
+            reader.readAsDataURL(files[0]);
+
+            reader.onloadend = function () {
+                holder.classList.add("uploadInProgress");
+                holder.querySelector(".pic").src = this.result;
+                $("#Logo").val(this.result)
+
+                var loader = document.createElement("div");
+                loader.classList.add("upload-loader");
+                loader.innerHTML =
+                    '<div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div>';
+                holder.appendChild(loader);
+
+                setTimeout(function () {
+                    holder.classList.remove("uploadInProgress");
+                    loader.remove();
+
+                    var random = Math.random();
+                    if (random < 0.9) {
+                        wrapper.innerHTML +=
+                            '<div class="snackbar show" role="alert"><i class="fa fa-check-circle text-success"></i> Profile image updated successfully</div>';
+                        triggerInput.value = "";
+                        setTimeout(function () {
+                            wrapper.querySelector('[role="alert"]').remove();
+                        }, 3000);
+                    } else {
+                        holder.querySelector(".pic").src = currentImg;
+                        wrapper.innerHTML +=
+                            '<div class="snackbar show" role="alert"><i class="fa fa-times-circle text-danger"></i> There is an error while uploading! Please try again later.</div>';
+                        triggerInput.value = "";
+                        setTimeout(function () {
+                            wrapper.querySelector('[role="alert"]').remove();
+                        }, 3000);
+                    }
+                }, 1500);
+            };
+        } else {
+            wrapper.innerHTML +=
+                '<div class="alert alert-danger d-inline-block p-2 small" role="alert">Please choose a valid image.</div>';
+            setTimeout(function () {
+                var invalidAlert = wrapper.querySelector('[role="alert"]');
+                if (invalidAlert) {
+                    invalidAlert.remove();
+                }
+            }, 3000);
+        }
     }
-   
+});
+
+$("#company-form").on("submit", function (e) {
+    event.preventDefault();
+    let form = $(e.target);
+    let json = convertFormToJSON(form);
+    submitForm(json);
+});
+
+function convertFormToJSON(form) {
+    return $(form)
+        .serializeArray()
+        .reduce(function (json, { name, value }) {
+            json[name] = value;
+            return json;
+        }, {});
+}
+
+
+function submitForm(formData) {
+
+    let headers = {};
+    let antiForgeryToken = $("input[name=__RequestVerificationToken]").val();
+
+    headers['RequestVerificationToken'] = antiForgeryToken;
+
+    $.ajax({
+        url: "api/Companies/save",
+        headers: headers,
+        contentType: "application/json",
+        type: "POST",
+        dataType: "json",
+        data: JSON.stringify(formData),
+        success: function (data) {
+           alert("Suucess")
+        },
+        error: function (xhr, textStatus, errorThrown) {
+           
+            alert('Request Status: ' + xhr.status + '; Status Text: ' + textStatus + '; Error: ' + errorThrown);
+        }
+    });
+
+
+
 
 
 
