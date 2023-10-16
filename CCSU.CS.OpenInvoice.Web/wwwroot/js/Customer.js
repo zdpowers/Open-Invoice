a00ccbe243c81d641046b1bd6a054ba086c770dd
Jesse Burlison <JesseBurlison@gmail.com>
2023-10-15 21:13:07 -0400

    updated customer controller to get the serialized javascript, also updated customer.js to get the data

--- CCSU.CS.OpenInvoice.Web/wwwroot/js/Customer.js
+++ CCSU.CS.OpenInvoice.Web/wwwroot/js/Customer.js
@@ -1,16 +1,38 @@
-$('#example').DataTable({
-    ajax: './js/SampleCustomers.txt',
-    columns: [
-        { data: 'CustomerId' },
-        { data: 'Name' },
-        { data: 'Contact'},
-        { data: 'Address' },
-        { data: 'Address2' },
-        { data: 'City' },
-        { data: 'State' },
-        { data: 'Zip' },
-        { data: 'Country' },
-        { data: 'Phone' },
-        { data: 'Email' }
-    ]
+//$('#example').DataTable({
+//    ajax: './../Models/SampleData/SampleCustomers.txt',
+//    columns: [
+//        { data: 'CustomerId' },
+//        { data: 'Name' },
+//        { data: 'Contact'},
+//        { data: 'Address' },
+//        { data: 'Address2' },
+//        { data: 'City' },
+//        { data: 'State' },
+//        { data: 'Zip' },
+//        { data: 'Country' },
+//        { data: 'Phone' },
+//        { data: 'Email' }
+//    ]
+//});
+
+$(document).ready(function (){
+    $('#example').DataTable({
+        "ajax": {
+            "url": "/api/customers",
+            "dataSrc": ""
+        },
+        columns: [
+            { data: 'CustomerId' },
+            { data: 'Name' },
+            { data: 'Contact' },
+            { data: 'Address' },
+            { data: 'Address2' },
+            { data: 'City' },
+            { data: 'State' },
+            { data: 'Zip' },
+            { data: 'Country' },
+            { data: 'Phone' },
+            { data: 'Email' }
+        ]
+    });
 });
