f4bb953da55b977e5a6d07a7732a760b05593c0e
Zack Powers <zacherydpowers@gmail.com>
2023-10-17 14:44:56 -0400

    initial invoice page front-end

---
+++ CCSU.CS.OpenInvoice.Web/Controllers/InvoiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CCSU.CS.OpenInvoice.Web.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
