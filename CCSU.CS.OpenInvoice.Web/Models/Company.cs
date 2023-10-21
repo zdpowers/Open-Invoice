cb6a0116cb72f0476079258a4bca8922984bbab6
yassir-y <90559530+yassir-y@users.noreply.github.com>
2023-10-21 13:02:26 -0400

    Started work on invoice screen

--- CCSU.CS.OpenInvoice.Web/Models/Company.cs
+++ CCSU.CS.OpenInvoice.Web/Models/Company.cs
@@ -4,7 +4,7 @@ namespace CCSU.CS.OpenInvoice.Web.Models
 {
     public class Company
     {
-        public int CompanyId { get; set; }
+        public int Id { get; set; }
 
         [Required]
         public string? Name { get; set; }
--- CCSU.CS.OpenInvoice.Web/Models/Customer.cs
+++ CCSU.CS.OpenInvoice.Web/Models/Customer.cs
@@ -2,7 +2,7 @@
 {
     public class Customer
     {
-        public int CustomerId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
 
         public string Contact { get; set; }
