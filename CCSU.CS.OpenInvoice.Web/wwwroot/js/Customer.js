c57a08c5810ae45215ca5436aad91b511799e7ce
Jesse Burlison <JesseBurlison@gmail.com>
2023-10-25 12:27:43 -0400

    Added html for add customer modal

--- CCSU.CS.OpenInvoice.Web/wwwroot/js/Customer.js
+++ CCSU.CS.OpenInvoice.Web/wwwroot/js/Customer.js
@@ -15,7 +15,14 @@
 //    ]
 //});
 
-$(document).ready(function (){
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
+
+$(document).ready(function () {
     $('#example').DataTable({
         "ajax": {
             "url": "/api/customers",
