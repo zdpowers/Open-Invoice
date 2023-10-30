278a9ba7888e3992e7d267b751efa480a733dfda
Zack Powers <zacherydpowers@gmail.com>
2023-10-30 10:07:34 -0400

    initial veriosn of Invoice Datatable

--- CCSU.CS.OpenInvoice.Web/Controllers/InvoiceController.cs
+++ CCSU.CS.OpenInvoice.Web/Controllers/InvoiceController.cs
@@ -51,4 +51,29 @@ namespace CCSU.CS.OpenInvoice.Web.Controllers
 
 
     }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        private readonly InvoicingContext _invoiceingContext;
+        public InvoicesController(InvoicingContext invoicingContext)
+        {
+            _invoiceingContext = invoicingContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllInvoices()
+        {
+            try
+            {
+                var invoices = _invoiceingContext.Invoices;
+                return Ok(invoices);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
 }
--- CCSU.CS.OpenInvoice.Web/wwwroot/js/Invoice.js
+++ CCSU.CS.OpenInvoice.Web/wwwroot/js/Invoice.js
@@ -66,9 +66,27 @@ $(document).ready(function () {
         updateTotalAfterTax();
     });
 
- 
 
 
 
 
+
+});
+
+// Populate invoice datatable
+$.ajax({
+    'url': "/api/Invoices",
+    'method': "GET",
+    'content': 'application/json'
+}).done(function (data) {
+    $('#invoiceTable').dataTable({
+        "aaData": data,
+        "columns": [
+            { data: 'Id' },
+            { data: 'CustomerId' },
+            { data: 'Date' },
+            { data: 'DueDate' },
+            { data: 'PaymentTerms'}
+        ]
+    });
 });
