f46a952beff791c5c9fdf0850c5afdbe7b1840de
yassir-y <90559530+yassir-y@users.noreply.github.com>
2023-11-05 12:30:49 -0500

    Fixing invoice List screen

---
+++ CCSU.CS.OpenInvoice.Web/Controllers/InvoicesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CCSU.CS.OpenInvoice.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        private readonly InvoicingContext _invoiceingContext;
+        public InvoicesController(InvoicingContext invoicingContext)
+        {
+            _invoiceingContext = invoicingContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllInvoices()
+        {
+            try
+            {
+                var invoices = _invoiceingContext.Invoices;
+                return Ok(invoices);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
--- CCSU.CS.OpenInvoice.Web/wwwroot/js/Invoice.js
+++ CCSU.CS.OpenInvoice.Web/wwwroot/js/Invoice.js
@@ -73,20 +73,3 @@ $(document).ready(function () {
 
 });
 
-// Populate invoice datatable
-$.ajax({
-    'url': "/api/Invoices",
-    'method': "GET",
-    'content': 'application/json'
-}).done(function (data) {
-    $('#invoiceTable').dataTable({
-        "aaData": data,
-        "columns": [
-            { data: 'Id' },
-            { data: 'CustomerId' },
-            { data: 'Date' },
-            { data: 'DueDate' },
-            { data: 'PaymentTerms'}
-        ]
-    });
-});
