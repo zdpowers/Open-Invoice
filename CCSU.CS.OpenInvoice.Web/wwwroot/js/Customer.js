ca8d074dbd9d809aa5b4143db3b89655fb32f379
Jesse Burlison <JesseBurlison@gmail.com>
2023-11-05 13:53:31 -0500

    Revert "Edited the javascript and cshtml so that the table will autmoatically update on new customer creation, the form will clear, and the modal will close"
    This reverts commit 4928cbd7ce30f2f6da18eb1ecb571995a8147ba6.

--- CCSU.CS.OpenInvoice.Web/wwwroot/js/Customer.js
+++ CCSU.CS.OpenInvoice.Web/wwwroot/js/Customer.js
@@ -1,185 +1,9 @@
-//#region old code
+/*
+    References:
+    https://datatables.net/forums/discussion/47910/add-button-column
+    https://stackoverflow.com/questions/19981949/how-to-make-a-button-in-bootstrap-look-like-a-normal-link-in-nav-tabs
+*/
 
-///*
-//    References:
-//    https://datatables.net/forums/discussion/47910/add-button-column
-//    https://stackoverflow.com/questions/19981949/how-to-make-a-button-in-bootstrap-look-like-a-normal-link-in-nav-tabs
-//*/
-
-//const myModal = document.getElementById('myModal')
-//const myInput = document.getElementById('myInput')
-
-//myModal.addEventListener('shown.bs.modal', () => {
-//    myInput.focus()
-//})
-
-//var form = $("#customer-form");
-//form.validate();
-
-//$("#customer-form").on("submit", function (e) {
-//    event.preventDefault();
-//    if ($("#customer-form").valid()) {
-//        let form = $(e.target);
-//        let json = convertFormToJSON(form);
-//        submitForm(json);
-//    }
-//    $('#myModal').myModal('hide');
-//});
-
-///*$("#customer-form").on("edit", function () {
-//    console.log("Hello from editting modal!");
-//    //$("#customerName").val("Test Name :D");
-//});*/
-
-////$(document).ready(function () {
-////    $("#modalEdit").click(function () {
-////        $('#myModal').modal('show');
-////        $("#customerName").val("Test Name :D");
-////    });
-////});
-
-////function fillAddCustomerModal() {
-////    $('#myModal').modal('show');
-////    $("#customerName").val("Test Name :D");
-////}
-
-//function convertFormToJSON(form) {
-//    return $(form)
-//        .serializeArray()
-//        .reduce(function (json, { name, value }) {
-//            json[name] = value;
-//            return json;
-//        }, {});
-//}
-
-//function submitForm(formData) {
-//    console.log(formData);
-//    console.log(JSON.stringify(formData));
-//    let headers = {};
-//    let antiForgeryToken = $("input[name=__RequestVerificationToken]").val();
-
-//    headers['RequestVerificationToken'] = antiForgeryToken;
-
-//    $.ajax({
-//        url: "api/Customers/save",
-//        headers: headers,
-//        contentType: "application/json",
-//        type: "POST",
-//        dataType: "json",
-//        data: JSON.stringify(formData),
-//        success: function (data) {
-//            //alert("Success");
-//            //location.reload(true);
-//            //location.reload(true);
-//            $('#example').DataTable().ajax.reload();
-//            $('#customer-form')[0].reset();
-//        },
-//        error: function (xhr, textStatus, errorThrown) {
-//            alert('Request Status: ' + xhr.status + '; Status Text: ' + textStatus + '; Error: ' + errorThrown);
-//        }
-//    });
-//}
-
-//var oTable;
-
-//$.ajax({
-//    'url': "/api/Customers",
-//    'method': "GET",
-//    'contentType': 'application/json',
-//    'dataSrc': ""
-//}).done(function (data) {
-//    $('#example').dataTable({
-//        select: {
-//            style: 'single'
-//        },
-//        "initComplete": function (settings, json) {
-//            oTable = $('#example').dataTable();
-
-//            oTable.on('click', 'tbody tr', (e) => {
-//                let classList = e.currentTarget.classList;
-
-//                if (classList.contains('selected')) {
-//                    classList.remove('selected');
-//                }
-//                else {
-//                    //oTable.rows('.selected').nodes().each((row) => row.classList.remove('selected'));
-//                    classList.add('selected');
-//                }
-//            });
-
-//            document.querySelector('#ultimateEditButton').addEventListener('click', function () {
-//                //table.row('.selected').remove().draw(false);
-//                console.log(oTable.api().row('.selected').data().Id);
-//            });
-//            //alert('DataTables has finished its initialisation.');
-//        },
-//        "aaData": data,
-//        "columns": [
-//            { data: 'Id' },
-//            { data: 'Name' },
-//            { data: 'Contact' },
-//            { data: 'Address' },
-//            { data: 'Address2' },
-//            { data: 'City' },
-//            { data: 'State' },
-//            { data: 'Zip' },
-//            { data: 'Country' },
-//            { data: 'Phone' },
-//            { data: 'Email' }/*,
-//            {
-//                data: null,
-//                render: function (data, type, row) {
-//                    document.getElementById("example").addEventListener("click", function (e) {
-//                        const tgt = e.target;
-//                        if (tgt.classList.contains("editButton")) {
-//                            e.preventDefault(); // stop the button if not type=button 
-//                            console.log("Hello from A editButton!");
-//                            console.log(row);
-//                        }
-//                    });
-
-//                    return '<button type="edit" class="btn btn-link editButton" data-toggle="modal" data-target="#myModal" data-whatever="editing">Edit</button>';
-//                }
-//            }
-//            {
-//                data: null,
-//                render: function (data, type, row) {
-//                    $("#edit").click(function (e) {
-//                        //console.log($('#example').DataTable().row().data());
-//                        console.log(row);
-//                    });
-
-//                    counter = 1;
-//                    myButton = '';
-//                    row().forEach(myFunction);
-//                    function myFunction() {
-//                        myButton = '<button type="edit" id="edit'
-//                            + counter + '" class="btn btn-link" data-toggle="modal" data-target="#myModal" data-whatever="editing">Edit</button>';
-//                        counter += 1;
-//                        return myButton;
-//                    }
-
-//                    return '<button type="edit" id="edit'
-//                        + '' + '" class="btn btn-link" data-toggle="modal" data-target="#myModal" data-whatever="editing">Edit</button>';
-//                }
-//            }*/
-//        ]
-//    })/*.on('click', 'tbody tr', (e) => {
-//        let classList = e.currentTarget.classList;
-
-//        if (classList.contains('selected')) {
-//            classList.remove('selected');
-//        }
-//        else {
-//            rows('.selected').nodes().each((row1) => row1.classList.remove('selected'));
-//            classList.add('selected');
-//        }
-//    })*/;
-//});
-
-//#endregion
-
-//#region new code
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
@@ -196,10 +20,25 @@ $("#customer-form").on("submit", function (e) {
         let form = $(e.target);
         let json = convertFormToJSON(form);
         submitForm(json);
-        $('#myModal').hide(); //closes the modal
-        $('.modal-backdrop').hide(); //closes the backdrop
     }
 });
+
+/*$("#customer-form").on("edit", function () {
+    console.log("Hello from editting modal!");
+    //$("#customerName").val("Test Name :D");
+});*/
+
+//$(document).ready(function () {
+//    $("#modalEdit").click(function () {
+//        $('#myModal').modal('show');
+//        $("#customerName").val("Test Name :D");
+//    });
+//});
+
+//function fillAddCustomerModal() {
+//    $('#myModal').modal('show');
+//    $("#customerName").val("Test Name :D");
+//}
 
 function convertFormToJSON(form) {
     return $(form)
@@ -209,29 +48,6 @@ function convertFormToJSON(form) {
             return json;
         }, {});
 }
-
-$(document).ready(function () {
-    $('#example').DataTable({
-        ajax: {
-            'url': '/api/Customers',
-            'dataSrc': ''
-        },
-        columns: [
-            { data: 'Id' },
-            { data: 'Name' },
-            { data: 'Contact' },
-            { data: 'Address' },
-            { data: 'Address2' },
-            { data: 'City' },
-            { data: 'State' },
-            { data: 'Zip' },
-            { data: 'Country' },
-            { data: 'Phone' },
-            { data: 'Email' }
-        ]
-    });
-});
-
 
 function submitForm(formData) {
     console.log(formData);
@@ -249,16 +65,108 @@ function submitForm(formData) {
         dataType: "json",
         data: JSON.stringify(formData),
         success: function (data) {
-            $('#example').DataTable().ajax.reload(null, false); //reloads ajax table
-            $('#customer-form')[0].reset(); //clears the modal form
+            //alert("Success");
+            location.reload(true);
         },
         error: function (xhr, textStatus, errorThrown) {
             alert('Request Status: ' + xhr.status + '; Status Text: ' + textStatus + '; Error: ' + errorThrown);
         }
     });
+
 }
 
-//#endregion
+var oTable;
 
+$.ajax({
+    'url': "/api/Customers",
+    'method': "GET",
+    'contentType': 'application/json'
+}).done(function (data) {
+    $('#example').dataTable({
+        select: {
+            style: 'single'
+        },
+        "initComplete": function (settings, json) {
+            oTable = $('#example').dataTable();
 
+            oTable.on('click', 'tbody tr', (e) => {
+                let classList = e.currentTarget.classList;
 
+                if (classList.contains('selected')) {
+                    classList.remove('selected');
+                }
+                else {
+                    //oTable.rows('.selected').nodes().each((row) => row.classList.remove('selected'));
+                    classList.add('selected');
+                }
+            });
+
+            document.querySelector('#ultimateEditButton').addEventListener('click', function () {
+                //table.row('.selected').remove().draw(false);
+                console.log(oTable.api().row('.selected').data().Id);
+            });
+            //alert('DataTables has finished its initialisation.');
+        },
+        "aaData": data,
+        "columns": [
+            { data: 'Id' },
+            { data: 'Name' },
+            { data: 'Contact' },
+            { data: 'Address' },
+            { data: 'Address2' },
+            { data: 'City' },
+            { data: 'State' },
+            { data: 'Zip' },
+            { data: 'Country' },
+            { data: 'Phone' },
+            { data: 'Email' }/*,
+            {
+                data: null,
+                render: function (data, type, row) {
+                    document.getElementById("example").addEventListener("click", function (e) {
+                        const tgt = e.target;
+                        if (tgt.classList.contains("editButton")) {
+                            e.preventDefault(); // stop the button if not type=button 
+                            console.log("Hello from A editButton!");
+                            console.log(row);
+                        }
+                    });
+
+                    return '<button type="edit" class="btn btn-link editButton" data-toggle="modal" data-target="#myModal" data-whatever="editing">Edit</button>';
+                }
+            }
+            {
+                data: null,
+                render: function (data, type, row) {
+                    $("#edit").click(function (e) {
+                        //console.log($('#example').DataTable().row().data());
+                        console.log(row);
+                    });
+
+                    counter = 1;
+                    myButton = '';
+                    row().forEach(myFunction);
+                    function myFunction() {
+                        myButton = '<button type="edit" id="edit'
+                            + counter + '" class="btn btn-link" data-toggle="modal" data-target="#myModal" data-whatever="editing">Edit</button>';
+                        counter += 1;
+                        return myButton;
+                    }
+
+                    return '<button type="edit" id="edit'
+                        + '' + '" class="btn btn-link" data-toggle="modal" data-target="#myModal" data-whatever="editing">Edit</button>';
+                }
+            }*/
+        ]
+    })/*.on('click', 'tbody tr', (e) => {
+        let classList = e.currentTarget.classList;
+
+        if (classList.contains('selected')) {
+            classList.remove('selected');
+        }
+        else {
+            rows('.selected').nodes().each((row1) => row1.classList.remove('selected'));
+            classList.add('selected');
+        }
+    })*/;
+});
